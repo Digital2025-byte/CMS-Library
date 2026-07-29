51394d52b4d9db8ea65094451c796fc9301b6d83
Majd Saati <mhdmajdsaati@gmail.com>
2026-07-29 15:58:51 +0300

    Update AccordionContainer to enhance layout responsiveness
    - Increased maximum width of the AccordionContainer section from 4xl to 7xl for improved content display.
    - Adjusted padding for better spacing on larger screens.

--- src/app/cmsComponents/AccordionWithContent/components/AccordionContainer.jsx
+++ src/app/cmsComponents/AccordionWithContent/components/AccordionContainer.jsx
@@ -5,7 +5,7 @@ export default function AccordionContainer({ lang, dir, children }) {
       lang={lang}
       dir={dir || (lang === "ar" ? "rtl" : "ltr")}
     >
-      <section className="mx-auto w-full max-w-4xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+      <section className="mx-auto w-full max-w-7xl px-4 py-8 sm:px-6 sm:py-12 lg:px-12 lg:py-16">
         {children}
       </section>
     </div>
